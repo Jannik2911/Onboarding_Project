54001da1a07777f4292a29927e38559fcfffd37c
Jannik Linde <s061570@studium.hspv.nrw.de>
2024-05-10 13:35:24 +0200

    Update #2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Dashboard from "./Components/Dashboard";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
@@ -23,9 +22,6 @@ function App() {
           />
         </Routes>
       </BrowserRouter>
-=======
-      <Button variant="contained" onClick={(console.log("test"))}>Hello world4</Button>
->>>>>>> e7b2d7fd7951f68675c1a8113a9c564485c3d809
     </div>
   );
 }
