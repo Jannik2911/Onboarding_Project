f0b3891ee6b89d538de7c2a7da7c0d8012171096
Jannik Linde <s061570@studium.hspv.nrw.de>
2024-05-10 13:29:31 +0200

    Update #1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
+import "./App.css";
+import Login from "./Components/Login";
+import ForgotPassword from "./Components/ForgotPassword";
+import Footer from "./Components/Footer";
 
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mitarbeiterverwaltung from "./Components/Mitarbeiterverwaltung";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" onClick={(console.log("test"))}>Hello world1</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/forgotpassword" element={<ForgotPassword />} />
+          <Route
+            exact
+            path="/mitarbeiterverwaltung"
+            element={<Mitarbeiterverwaltung />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
